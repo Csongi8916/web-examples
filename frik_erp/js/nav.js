88a6093bdf79e77ddb0ab7d4c1ede4dc26b2047e
Komár Csongor <csongi8916@gmail.com>
2024-08-01 06:58:00 +0200

    Support smaller devices

--- frik_erp/js/nav.js
+++ frik_erp/js/nav.js
@@ -1,8 +1,21 @@
+const maxWidth = 1150;
 let hamburgerBtn = document.querySelector("#hamburger-btn");
 let sidebar = document.querySelector('.sidebar');
 let header = document.querySelector('header');
 
+function init() {
+  if (window.innerWidth < maxWidth) {
+    toggleActive();
+  }
+}
+
+init();
+
 hamburgerBtn.onclick = function () {
+  toggleActive();
+};
+
+function toggleActive() {
   const isActive = sidebar.classList.toggle("active");
   if (isActive) {
     sidebar.setAttribute("style", "min-width: 250px");
@@ -13,5 +26,4 @@ hamburgerBtn.onclick = function () {
     header.setAttribute("style", "width: 80px");
     header.setAttribute("style", "width: calc(100vw - 80px)");
   }
-  console.log(isActive);
-};
+}
